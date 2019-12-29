0d63e634054a9b3062ca2a08f2024885674ae720
EC2 Default User <ec2-user@ip-172-31-45-91.ap-northeast-1.compute.internal>
2019-12-29 05:23:01 +0000

    テーブル変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_28_013143) do
-
-  create_table "group_dones", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "text"
-    t.integer "user_id"
-    t.integer "group_id"
-  end
+ActiveRecord::Schema.define(version: 2019_12_28_012950) do
 
   create_table "group_todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
